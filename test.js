19e5d5471382a8a1c69fa7dffd2c6df26d28d494
Yoshi Jäger <github@jaeger.berlin>
2019-07-04 11:07:28 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,34 +2,34 @@
 
 require('mocha');
 const assert = require('assert');
-const isOdd = require('./');
+const isNotOdd = require('./');
 
-describe('isOdd', function() {
+describe('isNotOdd', function() {
   it('should return true if the number is odd:', function() {
-    assert(!isOdd(0));
-    assert(!isOdd(2));
-    assert(isOdd(1));
-    assert(isOdd(3));
-    assert(isOdd(-1));
-    assert(isOdd(-3));
-    assert(isOdd(1.0e0));
-    assert(isOdd(9007199254740991));
+    assert(isNotOdd(0));
+    assert(isNotOdd(2));
+    assert(!isNotOdd(1));
+    assert(!isNotOdd(3));
+    assert(!isNotOdd(-1));
+    assert(!isNotOdd(-3));
+    assert(!isNotOdd(1.0e0));
+    assert(!isNotOdd(9007199254740991));
   });
 
   it('should work with strings:', function() {
-    assert(!isOdd('0'));
-    assert(!isOdd('2'));
-    assert(isOdd('1'));
-    assert(isOdd('3'));
-    assert(isOdd('1.0e0'));
-    assert(isOdd('9007199254740991'));
+    assert(isNotOdd('0'));
+    assert(isNotOdd('2'));
+    assert(!isNotOdd('1'));
+    assert(!isNotOdd('3'));
+    assert(!isNotOdd('1.0e0'));
+    assert(!isNotOdd('9007199254740991'));
   });
 
   it('should throw an error when an invalid value is passed', function() {
-    assert.throws(() => isOdd(), /expected a number/);
-    assert.throws(() => isOdd('foo'), /expected a number/);
-    assert.throws(() => isOdd('1.1e0'), /expected an integer/);
-    assert.throws(() => isOdd('9007199254740992'), /value exceeds maximum safe integer/);
-    assert.throws(() => isOdd(9007199254740992), /value exceeds maximum safe integer/);
+    assert.throws(() => isNotOdd(), /expected a number/);
+    assert.throws(() => isNotOdd('foo'), /expected a number/);
+    assert.throws(() => isNotOdd('1.1e0'), /expected an integer/);
+    assert.throws(() => isNotOdd('9007199254740992'), /value exceeds maximum safe integer/);
+    assert.throws(() => isNotOdd(9007199254740992), /value exceeds maximum safe integer/);
   });
 });
